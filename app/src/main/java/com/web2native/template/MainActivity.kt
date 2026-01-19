acc023245cdb674489e400e896b8301aa3cdcd0c
Khoman Sahu <khomans@Khomans-MacBook-Air.local>
2026-01-20 03:43:30 +0530

    Initial Android template

---
+++ app/src/main/java/com/web2native/template/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.web2native.template
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        // 🔥 Hardcoded URL for now (later dynamic)
+        webView.loadUrl("https://example.com")
+    }
+}
